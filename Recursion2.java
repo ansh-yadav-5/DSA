0e034b0076cdc8a17cb5a06f3203db00098c8de8
Ansh Yadav <ansh@example.com>
2026-04-07 15:36:31 +0530

    7/april

---
+++ Recursion2.java
@@ -0,0 +1,16 @@
+// Print Numbers from 1 to 5.
+
+
+public class Recursion2 {
+    public static void printNumber(int n){
+    if (n == 6) {  
+      return;
+    }
+    System.out.println(n);
+    printNumber(n + 1);
+  }
+  public static void main(String[] args) {
+    int n = 1;
+    printNumber(n);
+  }
+}
